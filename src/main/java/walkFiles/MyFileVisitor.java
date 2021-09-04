77fbaff7fa3bc765cb641d2d1ba6b4cb9e2b7357
Oscar Bareev <oscar.bareev@gmail.com>
2021-09-04 22:49:23 +0300

    Almost

--- src/main/java/walkFiles/MyFileVisitor.java
+++ src/main/java/walkFiles/MyFileVisitor.java
@@ -1,13 +1,23 @@
 package walkFiles;
 
 import dataClass.Info;
-import org.apache.pdfbox.pdmodel.PDDocument;
 
-import java.io.IOException;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.pdmodel.PDPage;
+import org.apache.pdfbox.pdmodel.PDPageContentStream;
+import org.apache.pdfbox.pdmodel.common.PDRectangle;
+import org.apache.pdfbox.pdmodel.graphics.image.JPEGFactory;
+import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
+
+
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.*;
 import java.nio.file.*;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Stream;
 
 
 public class MyFileVisitor extends SimpleFileVisitor<Path> {
@@ -15,10 +25,7 @@ public class MyFileVisitor extends SimpleFileVisitor<Path> {
     private List<Info> list;
 
 
-
-    private int indCount = 0;//!!!Нужно сделать корректное отображение индексов
-
-
+    private int indCount = 1;
 
 
     public MyFileVisitor(List<Info> list) {
@@ -26,38 +33,56 @@ public class MyFileVisitor extends SimpleFileVisitor<Path> {
     }
 
 
+    Path pdfPath = null;
+    Path rtfPath = null;
+    String creditorName = "";
+    ArrayList<Path> jpgList = new ArrayList<>();
+
+    int stepsCount = 0;
+
+
+
+
     @Override
     public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException {
 
+        jpgList.clear();
+        creditorName = "";
+        pdfPath = null;
+        rtfPath = null;
 
-
-        return FileVisitResult.CONTINUE;
-    }
-
-
-
-    @Override
-    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs){
-
-
-        //Сохраняем пути к файлам
-        Path pdfPath = null;
-        Path rtfPath = null;
-        ArrayList<Path> jpgList = new ArrayList<>();
-        String creditorName = "";
-        //String index = "AAA" + indCount++;
-
-        if (file.toString().endsWith("pdf")) {
-            pdfPath = file;
+        String rootPath = dir.getRoot().toString();
+        String newFolder = rootPath + "Требования кредиторов (отредактированные)";
+        if (!Files.exists(Paths.get(newFolder))) {
+            Files.createDirectory(Paths.get(newFolder));
         }
 
-        if (file.toString().endsWith("rtf")) {
-            rtfPath = file;
+        if (stepsCount != 0){
+            Stream<Path> filePathStream = Files.walk(dir);
+            filePathStream
+                    .filter(Files::isRegularFile)
+                    .forEach(file -> {
+
+                        if (file.toString().endsWith("rtf")) {
+                            rtfPath = file;
+                        }
+
+                        if (file.toString().endsWith("pdf")) {
+                            pdfPath = file;
+                        }
+
+                        if (file.toString().endsWith("jpg")) {
+                            jpgList.add(file);
+                        }
+                    });
         }
 
-        if (file.toString().endsWith("jpg")) {
-            jpgList.add(file);
-        }
+
+
+
+
+
+
 
         if (rtfPath != null && !rtfPath.toString().toLowerCase().contains("расчет")) {
             creditorName = rtfPath.getFileName().toString()
@@ -67,74 +92,119 @@ public class MyFileVisitor extends SimpleFileVisitor<Path> {
                     .replace("материалы", "")
                     .replace("справка", "")
                     .replace(".rtf", "")
-                    .replace("Справка по установлению требований кредиторов", "")
+                    .replace("по установлению требований кредиторов", "")
                     .trim();
         }
 
-        if (pdfPath != null){
+
+        if (pdfPath != null) {
+
+            if (creditorName.equals("")) {
+                String[] pdfNameArr = pdfPath.getFileName().toString().split(" ");
+                creditorName = pdfNameArr[1].trim();
+            }
 
             try {
-
-
                 PDDocument doc = PDDocument.load(pdfPath.toFile());
                 int count = doc.getNumberOfPages();
                 doc.close();
 
-                indCount++;
-                String index = "AAA" + indCount++;
+                if (count > 1) {
+                    String index = indVisual(indCount++);
+                    String newFileName = "Копия требования кредитора с приложениями (" +
+                            index + ", " + creditorName + ") на " + count + "л..pdf";
 
-                String newFileName = "Копия требования кредитора с приложениями на " + count + "л. (" +
-                        index + " " + shortName(creditorName) + ").pdf";
+
+                    if (!Files.exists(Paths.get(newFolder + "\\" + newFileName))) {
+                        Files.copy(pdfPath, Paths.get(newFolder + "\\" + newFileName));
+                    }
 
 
-                String rootPath = file.getRoot().toString();
-                String newFolder = rootPath + "Требования кредиторов (отредактированные)";
+                    list.add(new Info(index, creditorName));
+                }
 
-                if (!Files.exists(Paths.get(newFolder))){
-                    Files.createDirectory(Paths.get(newFolder));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+
+        if (jpgList.size() > 0) {
+
+            String index = indVisual(indCount++);
+            String newFileName = "Копия требования кредитора с приложениями (" +
+                    index + ", " + creditorName + ") на " + jpgList.size() + "л..pdf";
+
+            try {
+                PDDocument doc = new PDDocument();
+
+                for (Path imgPass : jpgList) {
+
+                    InputStream in = new FileInputStream(imgPass.toString());
+                    BufferedImage bimg = ImageIO.read(in);
+
+                    float width = bimg.getWidth() ;
+                    float height = bimg.getHeight();
+                    PDPage page = new PDPage(new PDRectangle(width, height));
+                    doc.addPage(page);
+                    PDImageXObject  pdImageXObject = JPEGFactory.createFromImage(doc, bimg, 0.5f, 10);
+
+                    PDPageContentStream contentStream = new PDPageContentStream(doc, page);
+
+                    contentStream.drawImage(pdImageXObject, 0, 0);
+
+                    contentStream.close();
+
+
+
+                   /* PDPage blankPage = new PDPage();
+                    document.addPage(blankPage);
+                    BufferedImage img = ImageIO.read(new File(imgPass.toString()));
+                    PDJpeg jpeg = new PDJpeg(document, img);
+                    PageFormat pf = document.getPageFormat(0);
+                    double pageWidth = pf.getWidth();
+                    double pageHeight = pf.getHeight();
+                    PDPageContentStream contentStream = new PDPageContentStream(document, blankPage);
+                    contentStream.drawImage(jpeg, 0, 0);
+                    contentStream.drawXObject(jpeg, 0, 0, (float) pageWidth, (float) pageHeight);
+                    contentStream.close();*/
+                }
+
+                if (!Files.exists(Paths.get(newFolder + "\\" + newFileName))) {
+                    doc.save(newFolder + "\\" + newFileName);
                 }
 
 
-                Files.copy(pdfPath, Paths.get(newFolder + "\\" + newFileName));
+                doc.close();
 
                 list.add(new Info(index, creditorName));
 
             } catch (IOException e) {
                 e.printStackTrace();
             }
-
-
         }
 
 
 
-        //Нужно, чтобы все работало, в том числе обработка jpeg
+        stepsCount++;
 
 
         return FileVisitResult.CONTINUE;
     }
 
 
-    private String shortName(String name){
+    private String indVisual(int indCount) {
 
-        StringBuilder changedName = new StringBuilder(name);
+        String indStr = indCount + "";
+        int intSize = indStr.length();
 
-        String[] nameArr = name.split(" ");
+        String result = "";
 
-        if (!name.toLowerCase().contains("ооо")){
-            if (nameArr.length >= 3){
-
-                changedName = new StringBuilder(nameArr[0].trim() + " ");
-
-               for (int i = 1; i <= nameArr.length; i++){
-                   changedName.append(nameArr[i].charAt(0)).append(".");
-                }
-            }
+        for (int i = intSize; i < 5; i++) {
+            result = result + "0";
         }
+        result = "AAA" + result + indCount;
 
-        return changedName.toString();
-
+        return result;
     }
-
-
 }
